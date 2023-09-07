174e97f119da2757e7f8e3b3cf36434cf131e932
Felipe Neves <soufelipeneves@gmail.com>
2023-09-07 00:20:03 -0300

    Tokens de tema finalizados

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,16 +1,34 @@
 const myTheme = {
     COLORS: {
+      RED_DARK: '#BF3B44',
+      RED_MID: '#F3BABD',
+      RED_LIGHT: '#F4E6E7',
+
+      GREEN_DARK: '#639339',
+      GREEN_MID: '#CBE4B4',
+      GREEN_LIGHT: '#E5F0DB',
+
+      GREEN_100: '#1B1D1E',
+      GREEN_200: '#333638',
+      GREEN_300: '#5C6265',
+      GREEN_400: '#B9BBBC',
+      GREEN_500: '#DDDEDF',
+      GREEN_600: '#EFF0F0',
+      GREEN_700: '#FAFAFA',
+
       WHITE: '#FFFFFF',
     },
     FONT_FAMILY: {
-      REGULAR: 'Roboto_400Regular',
-      BOLD: 'Roboto_700Bold'
+      REGULAR: 'NunitoSans_400Regular',
+      BOLD: 'NunitoSans_700Bold'
     },
     FONT_SIZE: {
+      XS: 12,
       SM: 14,
       MD: 16,
       LG: 18,
-      XL: 24
+      XL: 24,
+      XXL: 32
     }
   };
   
