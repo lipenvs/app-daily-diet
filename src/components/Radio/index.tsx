bbd444f14adef98eb3080c4da048c8e01037a554
Felipe Neves <soufelipeneves@gmail.com>
2023-10-21 21:20:14 -0300

    Correção no component radio

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,18 +1,32 @@
 import { myTheme } from "@/theme";
 import { CircleIcon, Container, Label, Option, Row } from "./styles";
+import { useState } from "react";
 
 export function Radio() {
+  const [isOptionSelected, setIsOptionSelected] = useState(false);
+
+  function handleOptionClick(value: boolean) {
+    setIsOptionSelected(value);
+  }
+
   return (
     <Container>
       <Label>Está dentro da dieta?</Label>
 
       <Row>
-        <Option isActive={false}>
+        <Option
+          isActive={isOptionSelected}
+          onPress={() => handleOptionClick(true)}
+        >
           <CircleIcon color={myTheme.COLORS.GREEN_DARK} />
           <Label>Sim</Label>
         </Option>
 
-        <Option isActive={true} isDanger>
+        <Option
+          isActive={!isOptionSelected}
+          isDanger
+          onPress={() => handleOptionClick(false)}
+        >
           <CircleIcon color={myTheme.COLORS.RED_DARK} />
           <Label>Não</Label>
         </Option>
